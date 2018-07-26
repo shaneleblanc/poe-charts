c1f7db3639d5085d2ce431fe815f56c60815dc00
Shane LeBlanc <shane@xs.vc>
2018-07-25 21:30:20 -0700

    cors proxy cleaner object literal

--- js/main.js
+++ js/main.js
@@ -2,19 +2,19 @@ const chaosHeight = 100;
 var chaosIcon = '';
 var exaltPrice = 60;
 var exaltIcon = '';
+
+
 function updateRates(){
   var result;
   console.log('I\'m updating the rates!!');
   dataSent = {
     mode: "cors", // no-cors, cors, *same-origin
         headers: {
-            "Origin": "https://shaneleblanc.github.io/poe-charts",
             "Content-Type": "application/json; charset=utf-8",
-            "Access-Control-Allow-Origin": "*"
-            // "Content-Type": "application/x-www-form-urlencoded",
         },
   }
-  fetch('currency.json', dataSent)
+
+  fetch('https://cors-anywhere.herokuapp.com/https://poe.ninja/api/Data/GetCurrencyOverview?league=tmphardcore', dataSent)
     .then(response => {
       return response.json();
     }).then(data => result = data)
@@ -26,10 +26,20 @@ function updateRates(){
     .catch(err => {
       console.log("It didn't work!!", err);
     });
+    /*
+    var client = new HttpClient();
+    client.get('https://poe.ninja/api/Data/GetCurrencyOverview?league=tmphardcore', function(response) {
+    console.log("we have data:", response);
+rates    rates = parse(response);
+    renderGraphs(rates);
+});
+*/
+
     console.log('returning result:',result)
     return result;
 }
 function parse(data){
+  // Hard code for Chaos and Exalted Orbs
   chaosIcon = data.currencyDetails[0].icon;
   exaltIcon = data.currencyDetails[1].icon;
   exaltPrice = data.lines[2].chaosEquivalent;
@@ -53,112 +63,40 @@ function parse(data){
     'BlessingofTul': data.lines[17].chaosEquivalent,
     'SplinterofChayula': data.lines[18].chaosEquivalent,
     'RegalOrb': data.lines[19].chaosEquivalent,*/
+
   let result = [
-    { name: 'Gemcutter\'s Prism',
-      chaosEquivalent: data.lines[data.lines.findIndex(function(currency) {return currency.currencyTypeName === 'Gemcutter\'s Prism' })].chaosEquivalent,
-      icon: data.currencyDetails[data.currencyDetails.findIndex(function(currency){ return currency.name === 'Gemcutter\'s Prism' })].icon
-    },
-    { name: 'Vaal Orb',
-      chaosEquivalent: data.lines[22].chaosEquivalent,
-      icon: data.currencyDetails[7].icon
-    },
-    { name: 'Orb of Regret',
-      chaosEquivalent: data.lines[20].chaosEquivalent,
-      icon: data.currencyDetails[8].icon
-    },
-    { name: 'Orb of Fusing',
-      chaosEquivalent: data.lines[21].chaosEquivalent,
-      icon: data.currencyDetails[4].icon
-    },
-    { name: 'Orb of Scouring',
-      chaosEquivalent: data.lines[24].chaosEquivalent,
-      icon: data.currencyDetails[13].icon
-    },
-    { name: 'Blessed Orb',
-      chaosEquivalent: data.lines[23].chaosEquivalent,
-      icon: data.currencyDetails[17].icon
-    },
-    { name: 'Orb of Alchemy',
-      chaosEquivalent: data.lines[26].chaosEquivalent,
-      icon: data.currencyDetails[3].icon
-    },
-    { name: 'Cartographer\'s Chisel',
-      chaosEquivalent: data.lines[25].chaosEquivalent,
-      icon: data.currencyDetails[9].icon
-    },
-    { name: 'Glassblower\'s Bauble',
-      chaosEquivalent: data.lines[28].chaosEquivalent,
-      icon: data.currencyDetails[19].icon
-    },
-    { name: 'Splinter of Xoph',
-      chaosEquivalent: data.lines[29].chaosEquivalent,
-      icon: data.currencyDetails[55].icon
-    },
-    { name: 'Silver Coin',
-      chaosEquivalent: data.lines[30].chaosEquivalent,
-      icon: data.currencyDetails[11].icon
-    },
-    { name: 'Orb of Binding',
-      chaosEquivalent: data.lines[27].chaosEquivalent,
-      icon: data.currencyDetails[73].icon
-    },
-    { name: 'Jewellers Orb',
-      chaosEquivalent: data.lines[31].chaosEquivalent,
-      icon: data.currencyDetails[10].icon
-    },
-    { name: 'Splinter of UulNetol',
-      chaosEquivalent: data.lines[32].chaosEquivalent,
-      icon: data.currencyDetails[58].icon
-    },
-    { name: 'Orb of Alteration',
-      chaosEquivalent: data.lines[33].chaosEquivalent,
-      icon: data.currencyDetails[5].icon
-    },
-    { name: 'Chromatic Orb',
-      chaosEquivalent: data.lines[34].chaosEquivalent,
-      icon: data.currencyDetails[16].icon
-    },
-    { name: 'Orb of Chance',
-      chaosEquivalent: data.lines[35].chaosEquivalent,
-      icon: data.currencyDetails[15].icon
-    },
-    { name: 'Splinter of Esh',
-      chaosEquivalent: data.lines[36].chaosEquivalent,
-      icon: data.currencyDetails[57].icon
-    },
-    { name: 'Splinter of Tul',
-      chaosEquivalent: data.lines[37].chaosEquivalent,
-      icon: data.currencyDetails[56].icon
-    },
-    { name: 'Orb of Augmentation',
-      chaosEquivalent: data.lines[38].chaosEquivalent,
-      icon: data.currencyDetails[19].icon
-    },
-    { name: 'Orb of Transmutation',
-      chaosEquivalent: data.lines[39].chaosEquivalent,
-      icon: data.currencyDetails[20].icon
-    },
-    { name: 'Blacksmith\'s Whetstone',
-      chaosEquivalent: data.lines[40].chaosEquivalent,
-      icon: data.currencyDetails[24].icon
-    },
-    { name: 'Portal Scroll',
-      chaosEquivalent: data.lines[41].chaosEquivalent,
-      icon: data.currencyDetails[23].icon
-    },
-    { name: 'Armourer\'s Scrap',
-      chaosEquivalent: data.lines[42].chaosEquivalent,
-      icon: data.currencyDetails[25].icon
-    },
-    { name: 'Perandus Coin',
-      chaosEquivalent: data.lines[43].chaosEquivalent,
-      icon: data.currencyDetails[12].icon
-    },
-    { name: 'Scroll of Wisdom',
-      chaosEquivalent: data.lines[44].chaosEquivalent,
-      icon: data.currencyDetails[22].icon
-    }
+    { name: 'Gemcutter\'s Prism' },
+    { name: 'Vaal Orb' },
+    { name: 'Orb of Regret' },
+    { name: 'Orb of Fusing' },
+    { name: 'Orb of Scouring' },
+    { name: 'Blessed Orb' },
+    { name: 'Orb of Alchemy' },
+    { name: 'Cartographer\'s Chisel' },
+    { name: 'Glassblower\'s Bauble' },
+    { name: 'Splinter of Xoph' },
+    { name: 'Silver Coin' },
+    { name: 'Orb of Binding' },
+    { name: 'Jeweller\'s Orb' },
+    { name: 'Splinter of Uul-Netol' },
+    { name: 'Orb of Alteration' },
+    { name: 'Chromatic Orb' },
+    { name: 'Orb of Chance' },
+    { name: 'Splinter of Esh' },
+    { name: 'Splinter of Tul' },
+    { name: 'Orb of Augmentation' },
+    { name: 'Orb of Transmutation' },
+    { name: 'Blacksmith\'s Whetstone' },
+    { name: 'Portal Scroll' },
+    { name: 'Armourer\'s Scrap' },
+    { name: 'Perandus Coin' },
+    { name: 'Scroll of Wisdom' }
 ];
+for (let item of result){ // Pulls data into the array of objects above
+  console.log(item.name);
+  item.chaosEquivalent = data.lines[data.lines.findIndex(function(currency) {return currency.currencyTypeName === item.name })].chaosEquivalent,
+  item.icon = data.currencyDetails[data.currencyDetails.findIndex(function(currency){ return currency.name === item.name })].icon
+}
   return result;
 }
 function renderGraphs(rates){
