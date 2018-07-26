5a760eaf8a62d2692067384459aa5d5c5f388c7c
Shane LeBlanc <shane@xs.vc>
2018-07-25 17:32:37 -0700

    added item info mouseovers

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const chaosHeight = 100;
 var chaosIcon = '';
+var exaltPrice = 60;
+var exaltIcon = '';
 function updateRates(){
   var result;
   console.log('I\'m updating the rates!!');
@@ -28,7 +30,13 @@ function updateRates(){
 }
 function parse(data){
   chaosIcon = data.currencyDetails[0].icon;
+  exaltIcon = data.currencyDetails[1].icon;
+  exaltPrice = data.lines[2].chaosEquivalent;
   chaosimage = document.getElementById('chaosIcon');
+  let itemInfo = document.getElementById('Chaos-info');
+  itemInfo.innerHTML = `
+  <div class="infoline">1 <img src="${exaltIcon}" width="32" height="32"> = ${exaltPrice} <img src="${chaosIcon}" width="32" height="32"></div>
+  `;
   chaosimage.src = chaosIcon;
 /*    { name: 'Blessing of Esh',
       chaosEquivalent: data.lines[8].chaosEquivalent,
@@ -46,51 +54,51 @@ function parse(data){
     'RegalOrb': data.lines[19].chaosEquivalent,*/
   let result = [
     { name: 'Gemcutter\'s Prism',
-      chaosEquivalent: data.lines[19].chaosEquivalent,
-      icon: data.currencyDetails[14].icon
+      chaosEquivalent: data.lines[data.lines.findIndex(function(currency) {return currency.currencyTypeName === 'Gemcutter\'s Prism' })].chaosEquivalent,
+      icon: data.currencyDetails[data.currencyDetails.findIndex(function(currency){ return currency.name === 'Gemcutter\'s Prism' })].icon
     },
     { name: 'Vaal Orb',
-      chaosEquivalent: data.lines[20].chaosEquivalent,
+      chaosEquivalent: data.lines[22].chaosEquivalent,
       icon: data.currencyDetails[7].icon
     },
     { name: 'Orb of Regret',
-      chaosEquivalent: data.lines[21].chaosEquivalent,
-      icon: data.currencyDetails[1].icon
+      chaosEquivalent: data.lines[20].chaosEquivalent,
+      icon: data.currencyDetails[8].icon
     },
     { name: 'Orb of Fusing',
-      chaosEquivalent: data.lines[22].chaosEquivalent,
+      chaosEquivalent: data.lines[21].chaosEquivalent,
       icon: data.currencyDetails[4].icon
     },
     { name: 'Orb of Scouring',
-      chaosEquivalent: data.lines[23].chaosEquivalent,
+      chaosEquivalent: data.lines[24].chaosEquivalent,
       icon: data.currencyDetails[13].icon
     },
     { name: 'Blessed Orb',
-      chaosEquivalent: data.lines[24].chaosEquivalent,
+      chaosEquivalent: data.lines[23].chaosEquivalent,
       icon: data.currencyDetails[17].icon
     },
     { name: 'Orb of Alchemy',
-      chaosEquivalent: data.lines[25].chaosEquivalent,
+      chaosEquivalent: data.lines[26].chaosEquivalent,
       icon: data.currencyDetails[3].icon
     },
     { name: 'Cartographer\'s Chisel',
-      chaosEquivalent: data.lines[26].chaosEquivalent,
+      chaosEquivalent: data.lines[25].chaosEquivalent,
       icon: data.currencyDetails[9].icon
     },
     { name: 'Glassblower\'s Bauble',
-      chaosEquivalent: data.lines[27].chaosEquivalent,
+      chaosEquivalent: data.lines[28].chaosEquivalent,
       icon: data.currencyDetails[19].icon
     },
     { name: 'Splinter of Xoph',
-      chaosEquivalent: data.lines[28].chaosEquivalent,
+      chaosEquivalent: data.lines[29].chaosEquivalent,
       icon: data.currencyDetails[55].icon
     },
     { name: 'Silver Coin',
-      chaosEquivalent: data.lines[29].chaosEquivalent,
+      chaosEquivalent: data.lines[30].chaosEquivalent,
       icon: data.currencyDetails[11].icon
     },
     { name: 'Orb of Binding',
-      chaosEquivalent: data.lines[30].chaosEquivalent,
+      chaosEquivalent: data.lines[27].chaosEquivalent,
       icon: data.currencyDetails[73].icon
     },
     { name: 'Jewellers Orb',
@@ -121,7 +129,7 @@ function parse(data){
       chaosEquivalent: data.lines[37].chaosEquivalent,
       icon: data.currencyDetails[56].icon
     },
-    { name: 'Orb of ugmentation',
+    { name: 'Orb of Augmentation',
       chaosEquivalent: data.lines[38].chaosEquivalent,
       icon: data.currencyDetails[19].icon
     },
@@ -155,15 +163,27 @@ function parse(data){
 function renderGraphs(rates){
   let chart = document.getElementById("chart");
   for (var item of rates){
-    console.log(item);
-    chaosEquiv = item.chaosEquivalent;
+    let unspacedName = item.name.replace(/\'/g,'').replace(/\s/g,'');
+    let chaosEquiv = item.chaosEquivalent;
     let barItem = document.createElement("div");
+    let itemInfo = document.createElement("div");
+    let worth = (1 / chaosEquiv).toFixed(1);
+    itemInfo.classList.add('info');
+    itemInfo.innerHTML = `
+    <div class="infoline">10 <img src="${chaosIcon}" width="32" height="32"> = ${Math.round(10 / chaosEquiv)} <img src="${item.icon}" width="32" height="32"></div> <br>
+    <div class="infoline">1&nbsp;&nbsp; <img src="${exaltIcon}" width="32" height="32"> = ${Math.round(exaltPrice * worth)} <img src="${item.icon}" width="32" height="32"></div>
+    `;
+    itemInfo.id = unspacedName + "-info";
+    itemInfo.classList.add('hidden');
     barItem.classList.add("BarGraph-bar");
-    barItem.textContent = (1 / chaosEquiv).toFixed(1) +'x '+ item.name;
+    barItem.onmouseover = function(){ console.log('moused over',unspacedName); document.getElementById(unspacedName+'-info').classList.remove('hidden');};
+    barItem.onmouseleave = function(){console.log('moused out of',unspacedName); document.getElementById(unspacedName+'-info').classList.add('hidden');};
+    barItem.textContent = worth +'x \r\n'+ item.name;
     barItem.appendChild(document.createElement("br"));
     let barImage = document.createElement("img");
     barImage.src = item.icon;
     barItem.appendChild(barImage);
+    barItem.appendChild(itemInfo);
     barItem.style.height = 100 * chaosEquiv + "%";
     chart.appendChild(barItem);
   }
@@ -179,8 +199,19 @@ function render(){
   let chaosimage = document.createElement('img');
   chaosimage.id = "chaosIcon";
   bar.classList.add("BarGraph-bar");
+
+  bar.onmouseover = ""
   bar.style.height = chaosHeight + "%";
-  bar.textContent = "= 1 Chaos Orb";
+  bar.innerHTML = "1x<br>Chaos Orb<br>";
+
+  let itemInfo = document.createElement("div");
+  itemInfo.classList.add('info');
+  itemInfo.id = "Chaos-info";
+  itemInfo.classList.add('hidden');
+  bar.onmouseover = function(){ console.log('moused over chaos'); document.getElementById('Chaos-info').classList.remove('hidden');};
+  bar.onmouseleave = function(){console.log('moused out of chaos'); document.getElementById('Chaos-info').classList.add('hidden');};
+
   bar.appendChild(chaosimage);
+  bar.appendChild(itemInfo);
   chart.appendChild(bar);
 }
